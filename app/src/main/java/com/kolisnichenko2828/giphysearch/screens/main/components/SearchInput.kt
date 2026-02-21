af65e8a1d81a1af7919fc587b7a742a563a27834
kolisnichenko2828 <kolisnichenko2828@gmail.com>
2026-02-21 02:59:45 +0200

    feat: clear SearchInput focus when keyboard hides

--- app/src/main/java/com/kolisnichenko2828/giphysearch/screens/main/components/SearchInput.kt
+++ app/src/main/java/com/kolisnichenko2828/giphysearch/screens/main/components/SearchInput.kt
@@ -1,5 +1,6 @@
 package com.kolisnichenko2828.giphysearch.screens.main.components
 
+import android.view.ViewTreeObserver
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
@@ -10,11 +11,16 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.kolisnichenko2828.giphysearch.R
 
 @Composable
@@ -23,6 +29,23 @@ fun SearchInput(
     onQueryChange: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val focusManager = LocalFocusManager.current
+    val view = LocalView.current
+
+    DisposableEffect(view) {
+        val listener = ViewTreeObserver.OnGlobalLayoutListener {
+            val insets = ViewCompat.getRootWindowInsets(view)
+            val isKeyboardVisible = insets?.isVisible(WindowInsetsCompat.Type.ime())
+            isKeyboardVisible?.let { if (!it) focusManager.clearFocus() }
+        }
+
+        view.viewTreeObserver.addOnGlobalLayoutListener(listener)
+
+        onDispose {
+            view.viewTreeObserver.removeOnGlobalLayoutListener(listener)
+        }
+    }
+
     OutlinedTextField(
         value = query,
         onValueChange = onQueryChange,
